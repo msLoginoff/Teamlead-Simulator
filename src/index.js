71eb43edfd6edc86fcf4d6b2ee938d4e86fe9860
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2024-06-24 01:24:19 +0700

    add minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
